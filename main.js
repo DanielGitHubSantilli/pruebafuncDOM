8d67a3a8f68e780de305bc54028a1f314b404f36
DanielGitSantilli <danielsantilli1@hotmail.com>
2023-07-28 18:16:26 -0300

    Primer Prueba DOM

---
+++ main.js
@@ -0,0 +1,7 @@
+console.dir(document)
+let titulo = document.getElementsByClassName("")
+console.log(titulo.innertext)
+// titulo.innertext ="animate__animated animate__flashr"
+// console.log(titulo.innertext)
+
+// Console.log(document.getelementsByTagName("li")) 
